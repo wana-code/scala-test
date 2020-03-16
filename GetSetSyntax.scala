fd0eb2cfff1a4033499fdccb1463783861f15788
wana-code <cherish00331@gmail.com>
2020-03-16 16:54:59 +0900

    modify

--- GetSetSyntax.scala
+++ GetSetSyntax.scala
@@ -1,20 +1,29 @@
-class Point{
-	private var _x = 0
-	private var _y = 0
-	private val bound = 100
+import fiddle.Fiddle, Fiddle.println
+import scalajs.js
 
-	def x = _x
-	def x_= (newValue: Int): Unit = {
-		if(newValue < bound) _x = newValue else printWarning
-	}
-	def y = _y
-	def y_= (newValue: Int): Unit = {
-		if(newValue < bound) _y = newValue else printWarning
-	}
-	private def printWarning = println("WARNINNG: Out of bounds")
+@js.annotation.JSExportTopLevel("ScalaFiddle")
+object ScalaFiddle {
+  // $FiddleStart
+  class Point {
+    private var _x = 0
+    private var _y = 0
+    private val bound = 100
+  
+    def x = _x
+    def x_= (newValue: Int): Unit = {
+      if (newValue < bound) _x = newValue else printWarning
+    }
+  
+    def y = _y
+    def y_= (newValue: Int): Unit = {
+      if (newValue < bound) _y = newValue else printWarning
+    }
+  
+    private def printWarning = println("WARNING: Out of bounds")
+  }
+  
+  val point1 = new Point
+  point1.x = 99
+  point1.y = 101 // prints the warning
+  // $FiddleEnd
 }
-
-val point1 = new Point
-point1.x =99
-point1.y =101
-
