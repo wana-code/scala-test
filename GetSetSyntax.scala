084473ee670f57ba220556af2b3576a435363668
wana-code <cherish00331@gmail.com>
2020-03-16 16:44:44 +0900

    GetSet_Example

---
+++ GetSetSyntax.scala
@@ -0,0 +1,20 @@
+class Point{
+	private var _x = 0
+	private var _y = 0
+	private val bound = 100
+
+	def x = _x
+	def x_= (newValue: Int): Unit = {
+		if(newValue < bound) _x = newValue else printWarning
+	}
+	def y = _y
+	def y_= (newValue: Int): Unit = {
+		if(newValue < bound) _y = newValue else printWarning
+	}
+	private def printWarning = println("WARNINNG: Out of bounds")
+}
+
+val point1 = new Point
+point1.x =99
+point1.y =101
+
